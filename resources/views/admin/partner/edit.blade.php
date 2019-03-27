76a46f3f8484c0e12c6cf7211ff7f0bb7a196efb
shubh97m <shubham.igniterpro@gmail.com>
2019-03-27 16:36:49 +0530

    changes

--- resources/views/admin/partner/edit.blade.php
+++ resources/views/admin/partner/edit.blade.php
@@ -5,7 +5,7 @@
     </div>
     <!-- /.box-header -->
     <div class="box-body">
- <form role="edit-partner" enctype="mulipart/form-data" method="PUT" action="{{url('admin/our-partners/'.___encrypt($partner['id'])) }}">
+ <form role="edit-partner" method="PUT" action="{{url('admin/our-partners/'.___encrypt($partner['id'])) }}">
   <input type="hidden" name="_method" value="PUT">
            <div class="form-group">
           <label>Partner's Image:</label>
