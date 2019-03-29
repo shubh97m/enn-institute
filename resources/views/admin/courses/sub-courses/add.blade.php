fa2dc0717d591b5ccd585e8ddae45dfc7e80cb07
shubh97m <shubham.igniterpro@gmail.com>
2019-03-29 18:02:48 +0530

    changes

--- resources/views/admin/courses/sub-courses/add.blade.php
+++ resources/views/admin/courses/sub-courses/add.blade.php
@@ -48,7 +48,7 @@
 
 
          <div class="box-footer">
-          <a href="{{url('admin/courses')}}" class="btn btn-default">Cancel</a>
+          <a href="{{url('admin/list-courses')}}" class="btn btn-default">Cancel</a>
           <button type="button" data-request="ajax-submit" data-target='[role="add-mainCourses"]' class="btn btn-info pull-right">Submit</button>
         </div>
       </form>
