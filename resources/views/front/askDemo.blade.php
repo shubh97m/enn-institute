ae3f4710d1044eb200a367c9a5840d5d1ed9730b
preetiverma123 <preetiverma123>
2019-03-30 12:27:08 +0530

    changes

--- resources/views/front/askDemo.blade.php
+++ resources/views/front/askDemo.blade.php
@@ -1,43 +1,45 @@
- <div class="container">
+<div class="container">
 	<h2 class="contact-info-size text-center mt-4 pd-4" >Get In Touch</h2>
+		<div class="askademo-wrapper">
 		<form role="contact" action="{{url('ask-a-demo')}}" method="POST">
 		{{csrf_field()}}
-		<div class="row form-group">
-			<div class="col-md-12">
-				<!-- <label for="fname">First Name</label> -->
-				<input type="text" name="name" id="name" class="form-control" placeholder="Your Name">
+			<div class="row form-group">
+				<div class="col-md-12">
+					<!-- <label for="fname">First Name</label> -->
+					<input type="text" name="name" id="name" class="form-control" placeholder="Your Name">
+				</div>
 			</div>
-		</div>
 
-		<div class="row form-group">
-			<div class="col-md-12">
-				<!-- <label for="email">Email</label> -->
-				<input type="text" name="email" id="email" class="form-control" placeholder="Your email address">
+			<div class="row form-group">
+				<div class="col-md-12">
+					<!-- <label for="email">Email</label> -->
+					<input type="text" name="email" id="email" class="form-control" placeholder="Your email address">
+				</div>
 			</div>
-		</div>
 
-		<div class="row form-group">
-			<div class="col-md-12">
-				<!-- <label for="message">Message</label> -->
-				<input type="text" name="mobile" id="mobile" class="form-control" placeholder="Your mobile number">	
+			<div class="row form-group">
+				<div class="col-md-12">
+					<!-- <label for="message">Message</label> -->
+					<input type="text" name="mobile" id="mobile" class="form-control" placeholder="Your mobile number">	
+				</div>
 			</div>
-		</div>
-		<div class="row form-group">
-	    	<div class="col-md-12">
-				  <select class="form-control" name="courses">
-				  	<option value="">-- Select Course Name--</option>
-				  	@foreach($course as $courses)
-			  <option value="{{!empty($courses['name'])?$courses['name']:''}}">{{!empty($courses['name'])?$courses['name']:''}}</option>	@endforeach
-				  </select>     				
-			</div>	  	
-		</div>
-		</div>
-
-		<div class="row form-group">
-		  <div class="col-md-5"></div>
-		   <div class="col-md-2">	
-				<button  type="button" data-request="ajax-submit" data-target='[role="contact"]' class="btn btn-primary text-center">Send Message</button>
+			<div class="row form-group">
+		    	<div class="col-md-12">
+					  <select class="form-control" name="courses">
+					  	<option value="">-- Select Course Name--</option>
+					  	@foreach($course as $courses)
+				  <option value="{{!empty($courses['name'])?$courses['name']:''}}">{{!empty($courses['name'])?$courses['name']:''}}</option>	@endforeach
+					  </select>     				
+				</div>	  	
 			</div>
-		   <div class="col-md-5"></div>	 
-		</div>
-	</form>	
+			
+			<div class="row form-group">
+			  
+			   <div class="col-md-12">	
+					<button  type="button" data-request="ajax-submit" data-target='[role="contact"]' class="btn btn-primary text-center">Send Message</button>
+				</div>
+			  
+			</div>
+		</form>	
+	</div>
+</div>
