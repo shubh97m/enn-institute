fbd93c06835c02a903251fc0a1dd52e346c3e420
preetiverma123 <preetiverma123>
2019-04-06 17:54:24 +0530

    changes

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -5,7 +5,7 @@
       <div class="user-panel">
         <div class="pull-left image">
 
-          <img src="{{url('assets/dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
+          <img src="{{url('assets/dist/img/user2-160x160.jpg')}}" alt="User Image">
         </div>
         <div class="pull-left info">
          
