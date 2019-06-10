ba4f0aa4b51085f9032ffde40289ab54c645a983
unknown <preetiverma16lko@gmail.com>
2019-06-10 18:06:36 +0530

    changes

--- resources/views/front/contact.blade.php
+++ resources/views/front/contact.blade.php
@@ -2,7 +2,7 @@
 		<div id="jsnn-contact">
 			<div class="container">
 				<div class="row">
-						<div class="col-md-6 animate-box pad-btm">
+						<div class="col-md-6 animate-box pad-btm" data-aos="fade-right">
 						<h2 class="contact-info-size">Get In Touch</h2>
 						<form role="contact" action="{{url('contact')}}" method="POST">
 							{{csrf_field()}}
@@ -42,7 +42,7 @@
 							</div>
 						</form>		
 					</div>
-					<div class="col-md-6  animate-box">
+					<div class="col-md-6  animate-box" data-aos="fade-left">
 						<h2 class="contact-info-size">Contact Information</h2>
 						<div class="row">
 							<div class="col-md-12">
@@ -73,7 +73,7 @@
 				</div>
 			</div>
 		</div>
-		<div id="" class="jsnn-map">
+		<div id="" class="jsnn-map" data-aos="fade-up">
 			<div class="container">
 				<div class="mapsection-contact">
 	           		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3501.6385473239793!2d77.29242101455978!3d28.640593290468118!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfb50f3139a0f%3A0x423c97213415e690!2sICICI+Bank+Preet+Vihar%2C+Delhi+-+Branch+%26+ATM!5e0!3m2!1sen!2sin!4v1552164127712" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
